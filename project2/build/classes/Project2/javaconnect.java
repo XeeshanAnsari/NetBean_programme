9689b6af1541223a1517265da943156e3f288d2c
zeeshan <zeeshan@gmail.com>
2016-08-03 23:32:38 -0700

    NetBean programme like calculator ,salary sheet etc

---
+++ project2/build/classes/Project2/javaconnect.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Project2;
+
+/**
+ *
+ * @author xeeshan
+ */
+import java.sql.*;
+import javax.swing.*;
+public class javaconnect {
+
+Connection conn=null;
+    public static Connection connecrDb(){
+        try{
+            
+            Class.forName("org.sqlite.JDBC");
+            Connection conn =DriverManager.getConnection("jdbc:sqlite:D:\\NetBeansProjects\\project1\\project1.sqlite");
+            JOptionPane.showMessageDialog(null, "Connection Estiblished");
+            return conn;
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+            
+        }
+    }
+
+}
